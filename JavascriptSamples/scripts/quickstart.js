1cb326d921faf1472291eb6aa1b5462b32bc57b2
SrinivasV18 <srinivasv18@gmail.com>
2018-10-29 12:54:56 +0530

    with callback examples

--- JavascriptSamples/scripts/quickstart.js
+++ JavascriptSamples/scripts/quickstart.js
@@ -2,10 +2,17 @@ var app ={};
 app.show = function(){
     console.log('hello world...');
 }
+app.updateContent = function(el,msg){
+    return function(){
+    var myHeading = document.getElementById(el);
+    myHeading.innerHTML =msg;
+    }
+}
 app.init =  function(){
     var headBtn = document.getElementById('showHeading');
-    var myHeading = document.getElementById('myHeading');
-    headBtn.addEventListener('click', function(){
-        myHeading.innerHTML ='Java script examples....';
-    })
+    var subheadBtn = document.getElementById('showSubHeading');
+    headBtn.addEventListener('click',app.updateContent('myHeading','javascript examples...'));
+    subheadBtn.addEventListener('click',app.updateContent('minorHeading','VDSI-Javascript...'));
 }
+app.show();
+app.init();
