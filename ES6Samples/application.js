194c6e79a735bf68072328512b9bac76f165f777
SrinivasV18 <srinivasv18@gmail.com>
2018-10-29 16:24:13 +0530

    static method examples

--- ES6Samples/application.js
+++ ES6Samples/application.js
@@ -1,4 +1,15 @@
 let LifeInsurance = require('./insurance');
-const nivas = new LifeInsurance(101,'Nivas',2323232);
-console.log(nivas.toString());
-console.log(nivas.getPolicyHolderName);
+class Application {
+    static main() {
+        const nivas = new LifeInsurance(101, 'Nivas', 2323232);
+        const venki = new LifeInsurance(102, 'Venki', 5656232);
+
+        const policyList = [nivas, venki];
+        for (let i = 0; i < policyList.length; i++) {
+            console.log(policyList[i].toString());
+            console.log(policyList[i].getPolicyHolderName);
+        }
+    }
+    //console.log(i);
+}
+Application.main();
--- ES6Samples/insurance.js
+++ ES6Samples/insurance.js
@@ -1,14 +1,17 @@
 class LifeInsurance {
     constructor(id, policyHolderName, policyAmount) {
         this.id = id;
-        this.policyHolderName = policyHolderName;
+        this._policyHolderName = policyHolderName;
         this.policyAmount = policyAmount;
     }
+    set policyHolderName(policyHolderName){
+        this._policyHolderName = policyHolderName;
+    }
     get getPolicyHolderName(){
-        return this.policyHolderName;
+        return this._policyHolderName.toUpperCase();
     }
     toString(){
-        return `${this.id},${this.policyHolderName},${this.policyAmount}`;
+        return `${this.id},${this.getPolicyHolderName},${this.policyAmount}`;
     }
 }
 module.exports = LifeInsurance;
