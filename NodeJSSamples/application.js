5aac86c47b078ff9d3f4062e1442e5727cddceb3
SrinivasV18 <srinivasv18@gmail.com>
2018-10-29 16:38:39 +0530

    static method and arrow functions.

--- NodeJSSamples/application.js
+++ NodeJSSamples/application.js
@@ -2,3 +2,4 @@ var bankModule = require('./bankAccount');
 //console.log(global);  -- in node.js , global variable is global.
 console.log(bankModule.simpleInterest());
 console.log(bankModule.coumpoundInterest());
+console.log(bankModule.specialInterest());
--- NodeJSSamples/bankAccount.js
+++ NodeJSSamples/bankAccount.js
@@ -2,7 +2,7 @@ var app = {};
 app.simpleInterest = function(){
     return 'Simple Interest';
 }
-
+app.specialInterest=() => 'Special interest';
 app.coumpoundInterest = function(){
     return 'Compound Interest';
 }
